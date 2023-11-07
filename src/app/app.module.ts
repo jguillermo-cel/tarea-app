13292a189e3bf99728d94636b6d9f56f0bff3163
julio.celestino.contractor <julio.celestino.contractor@bbva.com>
2023-11-07 16:44:29 -0500

    feat: delete useless components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,8 +14,6 @@ import { TaskComponent } from './components/task/task.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     DashboardComponent,
     LoginComponent,
     TaskComponent
